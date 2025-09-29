0a7a9cc2daca96f9daeb8b262ad668f78bdfc09a
Abhijit Dutta <134042042+AbhijitDestro@users.noreply.github.com>
2025-09-30 00:10:16 +0530

    hero section, navbar, file upload

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { formatSize } from '~/lib/utils';
+
+interface FileUploaderProps{
+  onFileSelect?:(file:File | null)=>void;
+}
+
+const FileUploader = ({onFileSelect}:FileUploaderProps) => {
+  const [file,setFile]=useState<File | null>(null)
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const selectedFile: File | null = acceptedFiles[0] || null
+    setFile(selectedFile)
+    onFileSelect?.(selectedFile)
+  }, [onFileSelect])
+
+  const {getRootProps, getInputProps,isDragActive,acceptedFiles}=useDropzone({onDrop, multiple:false,accept:{'application/pdf':['.pdf']},maxSize:20*1024*1024})
+  
+  const Files = acceptedFiles.map((file: File) => (
+    <div key={file.name}>
+      {file.name} - {file.size} bytes
+    </div>
+  ))
+
+  return (
+    <div className='w-full gradient-border'>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+            <div className='space-y-4 cursor-pointer'>
+
+            {file ?(
+              <div className='uploader-selected-file' onClick={(e)=>e.stopPropagation()}>
+                <div className='flex items-center space-x-3'>
+                  <img src="/images/pdf.png" className='size-10' alt="pdf" />
+                  <div>
+                    <p className='text-sm font-medium text-gray-700 truncate max-w-xs'>
+                       {file.name}
+                    </p>
+                    <p className='text-sm font-medium text-gray-500'>{formatSize(file.size)}</p>
+                  </div>
+                </div>
+                <button className='p-2 cursor-pointer' onClick={()=>{setFile(null);
+                onFileSelect?.(null)}}>
+                  <img src="/icons/cross.svg" className='w-4 h-4' alt="remove" />
+                </button>
+              </div>
+            ):(
+              <div>
+                <div className='mx-auto w-16 h-16 flex items-center justify-center mb-2'>
+                <img src="/icons/info.svg" className='size-20' alt="upload" />
+            </div>
+                  <p className='text-lg text-gray-500'>
+                      <span className='font-semibold'>
+                        Click to upload
+                      </span> or drag and drop
+                  </p>
+                  <p className='text-lg text-gray-500'>PDF (max 20 MB)</p>
+                </div>
+            )}
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default FileUploader
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,43 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/contants";
+import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SkillSnap" },
+    { name: "description", content: "AI-powered Resume Builder & Analyzer. Create job-winning resumes, optimize keywords, and boost your chances of landing your dream role" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const {isLoading, auth} = usePuterStore()
+  const navigate=useNavigate()
+
+  useEffect(() => {
+    if(!auth.isAuthenticated)
+      navigate("/auth?next=/")
+  },[auth.isAuthenticated, navigate])
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-10">
+         <h1>Build, Analyze & Perfect Your Resume in Minutes</h1>
+         <h2>Create professional resumes, checks ATS compatibility, and helps you land your dream job.</h2>
+      </div>
+    
+     
+     {resumes.length > 0 && (
+     <div className="resumes-section">
+    {resumes.map(resume => (
+      <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+    </section>
+  </main>;
 }
